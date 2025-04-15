e7819238b9f7afe01bde7b4ebad92458b939e48c
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2025-04-15 16:58:33 +0100

    bug fix

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <div className="h-auto flex bg-white">
-      <aside className="hidden md:block w-[38%]">
+      <aside className="hidden lg:block w-[38%]">
         <Navbar side={true} />
       </aside>
       <main>
