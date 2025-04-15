e680867327e52ec4fb23765193d4c4d1500f948e
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2025-04-15 09:26:35 +0100

    minor changes

--- website/src/app/record/layout.tsx
+++ website/src/app/record/layout.tsx
@@ -6,13 +6,8 @@ export default function RecordLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div>
-            <header>
-                <h1>Record Details Section</h1>
-            </header>
-            <main>
-                <Suspense>{children}</Suspense>
-            </main>
-        </div>
+        <main>
+            <Suspense>{children}</Suspense>
+        </main>
     );
 }
